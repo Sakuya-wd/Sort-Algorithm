334409369fa1200814eec17c3b97ab1bb02bb7e1
Sakuya-wd <yamamoto1.527@gmail.com>
2021-01-20 22:40:03 +0900

    ソートアルゴリズムを修正しました

--- src/RadixSortDisplay.js
+++ src/RadixSortDisplay.js
@@ -5,7 +5,6 @@ const RadixSortConfirm = require("./RadixSortConfirm.js");
 const RadixSortDisplay = () => {
   //ソート元のデータ。
   let array = [101, 103, 108, 102, 105, 100, 104, 109, 107, 106, 110, 200];
-
   console.log(`ソート前 ${array}`);
 
   let result = RadixSortConfirm(array);
