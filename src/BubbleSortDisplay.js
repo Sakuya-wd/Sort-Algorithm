44f7adfeb872fe07f3e137f47645528e5869ae90
Sakuya-wd <yamamoto1.527@gmail.com>
2021-01-20 22:40:03 +0900

    ソートアルゴリズムを修正しました

--- src/BubbleSortDisplay.js
+++ src/BubbleSortDisplay.js
@@ -5,7 +5,6 @@ const BubbleSortConfirm = require("./BubbleSortConfirm.js");
 const BubbleSortDisplay = () => {
   //ソート元のデータ。
   let array = [1, 3, 8, 2, 5, 0, 4, 9, 7, 6];
-
   console.log(`ソート前 ${array}`);
 
   let result = BubbleSortConfirm(array);
