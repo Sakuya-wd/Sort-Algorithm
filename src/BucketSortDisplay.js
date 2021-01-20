c2a9a1738db11fb967869ecd8af05612ac86a365
Sakuya-wd <yamamoto1.527@gmail.com>
2021-01-20 22:40:03 +0900

    FizzBuzzを修正しました

--- src/BucketSortDisplay.js
+++ src/BucketSortDisplay.js
@@ -5,7 +5,6 @@ const BucketSortConfirm = require("./BucketSortConfirm.js");
 const BucketSortDisplay = () => {
   //ソート元のデータ。
   let array = [1, 3, 8, 2, 5, 0, 4, 9, 7, 6];
-
   console.log(`ソート前 ${array}`);
 
   let result = BucketSortConfirm(array);
