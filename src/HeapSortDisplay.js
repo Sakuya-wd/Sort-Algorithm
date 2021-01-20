3fc496daa3e5214ae07c37add4eef284c9ba15e1
Sakuya-wd <yamamoto1.527@gmail.com>
2021-01-20 22:40:03 +0900

    ソートアルゴリズムを修正しました

--- src/HeapSortDisplay.js
+++ src/HeapSortDisplay.js
@@ -5,7 +5,6 @@ const HeapSortConfirm = require("./HeapSortConfirm.js");
 const HeapSortDisplay = () => {
   //ソート元のデータ。
   let array = [1, 3, 8, 2, 5, 0, 4, 9, 7, 6];
-
   console.log(`ソート前 ${array}`);
 
   let result = HeapSortConfirm(array);
